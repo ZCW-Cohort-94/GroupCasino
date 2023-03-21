058af6d9dfe6e9daf413b6a4cf4bc025cbfe1522
Nguyet <nguyet@zipcodersmbp103.mynetworksettings.com>
2023-03-20 21:07:50 -0400

    added structure for WheelThing

---
+++ src/main/java/com/github/zipcodewilmington/WheelThing.java
@@ -0,0 +1,10 @@
+package com.github.zipcodewilmington;
+
+import java.util.Random;
+
+public class WheelThing {
+//    public int numberGenerator(int lowerBound, int upperBound){
+//        Random rand = new Random();
+//        return rand.nextInt(upperBound-lowerBound)+lowerBound;
+//    }
+}
