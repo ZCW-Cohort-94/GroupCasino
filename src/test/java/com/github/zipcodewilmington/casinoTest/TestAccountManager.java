3d72ff9f9569abcf6d604840aaaaf957b90c0186
Nguyet <nguyet@Zipcoders-MacBook-Pro-103.local>
2023-03-22 14:34:49 -0400

    added tests for casino

---
+++ src/test/java/com/github/zipcodewilmington/casinoTest/TestAccountManager.java
@@ -0,0 +1,12 @@
+package com.github.zipcodewilmington.casinoTest;
+
+import org.junit.Test;
+
+public class TestAccountManager {
+    @Test
+    public void testCreateAccount(){
+        // Given
+        // When
+        // Then
+    }
+}
