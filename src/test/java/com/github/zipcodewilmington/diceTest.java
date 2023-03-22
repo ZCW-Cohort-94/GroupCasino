550fa627d04e5fa130b1eb30db4dc50aa85b7c8e
James <james@Zipcoders-MacBook-Pro-101.local>
2023-03-22 14:54:00 -0400

    made changes

--- src/test/java/com/github/zipcodewilmington/diceTest.java
+++ src/test/java/com/github/zipcodewilmington/diceTest.java
@@ -1,2 +1,39 @@
-package com.github.zipcodewilmington;public class diceTest {
+package com.github.zipcodewilmington;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.github.zipcodewilmington.casino.gameTools.Dice;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class diceTest {
+    @Test
+    public void testDiceConstructor(){
+        // Given
+        int expected = 2;
+        Dice dice = new Dice(expected);
+
+        // When
+        int actual = dice.getNumberDice();
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetNumberDice(){
+        // Given
+        int numDice = 4;
+        Dice dice = new Dice(numDice);
+        // When
+        int expected = 22;
+        dice.setNumberDice(expected);
+        int actual = dice.getNumberDice();
+        //
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testRolldice(){
+        // Given
+
+        // When
+        // Then
+    }
 }
