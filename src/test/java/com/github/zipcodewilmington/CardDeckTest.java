1eb76a0add0c62a64b657dfea629577ccbcf606a
adams <stilwelladam8@gmail.com>
2023-03-22 13:49:00 -0400

    Comments for the tests I will create.

--- src/test/java/com/github/zipcodewilmington/CardDeckTest.java
+++ src/test/java/com/github/zipcodewilmington/CardDeckTest.java
@@ -1,4 +1,8 @@
 package com.github.zipcodewilmington;
 
 public class CardDeckTest {
+    //test to see if 52 cards are created and stored
+
+    //test to see if every card is unique
+
 }
---
+++ src/test/java/com/github/zipcodewilmington/CardTest.java
@@ -0,0 +1,8 @@
+package com.github.zipcodewilmington;
+
+public class CardTest {
+    //test to see if a card gets a suit value
+
+    //test to see if a card gets a rank value
+
+}
