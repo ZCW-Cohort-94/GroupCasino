0c35e94c9bb7daddc67383a346623e7b481e7490
James <james@Zipcoders-MacBook-Pro-101.local>
2023-03-22 21:11:21 -0400

    trying to add 3 rows to slots

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -5,21 +5,45 @@ import java.util.Random;
  * Created by leon on 7/21/2020.
  */
 public class SlotsGame {
-    private String[] outcome = {"ZCW", "Bar", "cherry", "7", "coal", "bunny", "$"};
-    private int rollCount = 200;
-    int a, b, c;
+    private final String[] outcome = {"ZCW", "Bar", "cherry", "7", "coal", "bunny", "$"};
+    int a, b, c, d, e, f, g, h, j;
+    String x,x1, x2, y, y1, y2, z, z1, z2;
     Random rand;
-    public String pullSlots(){
+    public int getSlotRoll() {
+        return rand.nextInt(outcome.length);
+    }
+    public String pullSlots() throws InterruptedException {
         rand = new Random();
+        int rollCount = 200;
         for (int i = 0; i < rollCount; i++) {
-            a = rand.nextInt(outcome.length);
-            b = rand.nextInt(outcome.length);
-            c = rand.nextInt(outcome.length);
-            System.out.print("[" + outcome[a] + ":" + outcome[b] + ":" + outcome[c] + "]");
+            a = getSlotRoll();
+            b = getSlotRoll();
+            c = getSlotRoll();
+            d = getSlotRoll();
+            e = getSlotRoll();
+            f = getSlotRoll();
+            g = getSlotRoll();
+            h = getSlotRoll();
+            j = getSlotRoll();
+            x = outcome[a];
+            x1 = outcome[b];
+            x2 = outcome[c];
+            y = outcome[d];
+            y1 = outcome[e];
+            y2 = outcome[f];
+            z = outcome[g];
+            z1 = outcome[h];
+            z2 = outcome[j];
+            System.out.printf("[  " + "  %s  " + "  :  " + "  %s  " + "  :  " + "  %s  " +   "]\r\n", x,y,z);
+            Thread.sleep(600);
+            System.out.printf("[  " + "  %s  " + "  :  " + "  %s  " + "  :  " + "  %s  " +   "]\r", x1,y1,z1);
+            Thread.sleep(600);
+            System.out.printf("[  " + "  %s  " + "  :  " + "  %s  " + "  :  " + "  %s  " +   "]\r", x2, y2, z2);
+            Thread.sleep(600);
         }
         return null;
     }
-    public void run() {
+    public void run() throws InterruptedException {
         String pullSlot = pullSlots();
 
     }
