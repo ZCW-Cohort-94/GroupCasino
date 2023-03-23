8ad2c07b85d515b79b996010f30574110f98074e
adams <stilwelladam8@gmail.com>
2023-03-23 14:21:50 -0400

    Finished testing for card deck.

--- src/test/java/com/github/zipcodewilmington/gameToolsTest/CardDeckTest.java
+++ src/test/java/com/github/zipcodewilmington/gameToolsTest/CardDeckTest.java
@@ -1,5 +1,6 @@
 package com.github.zipcodewilmington.gameToolsTest;
 
+import com.github.zipcodewilmington.utils.Card;
 import org.junit.Assert;
 import org.junit.Test;
 import com.github.zipcodewilmington.casino.gameTools.CardDeck;
@@ -18,4 +19,24 @@ public class CardDeckTest {
     }
 
     //test to see if all cards do not repeat
+    @Test
+    public void createCardDeckTest2(){
+        //given
+        CardDeck cardDeck = new CardDeck();
+        Card[] deckArray = cardDeck.createCardDeck().toArray(new Card[0]);
+        //with the current loop, the counter will increase when the card equals itself, the first card is skipped
+        //there are 52 cards, so the counter should be 51
+        int expectedCount = 51;
+        int actualCount = 0;
+        //when
+        for(int i = 0; i <deckArray.length; i++){
+            for(int j = 1; j <deckArray.length; j++){
+                if(deckArray[i].getRank().equals(deckArray[j].getRank()) && deckArray[i].getSuit().equals(deckArray[j].getSuit())){
+                    actualCount++;
+                }
+            }
+        }
+        //then
+        Assert.assertEquals(expectedCount, actualCount);
+    }
 }
